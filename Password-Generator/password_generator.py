1ab2531fa05b1d7e8415ba9920bdb428b3b3ea93
veeraj007 <veeraj.s@icloud.com>
2026-01-01 19:41:29 +0530

    Added password generator skeleton

---
+++ Password-Generator/password_generator.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+
+def generate_password():
+    pass
+
+
+def main():
+    print("\n--- Password Generator ---")
+    generate_password()
+
+
+if __name__ == "__main__":
+    main()
